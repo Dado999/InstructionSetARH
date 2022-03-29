32188fb1303b23debe5a1d2a78b20b3a4a245ca5
damirfilipovic99@gmail.com <damirfilipovic999@gmail.com>
2022-03-29 19:23:07 +0200

    29.3.2022

--- InstructionSetARH/InstructionSetARH/Procesor.cpp
+++ InstructionSetARH/InstructionSetARH/Procesor.cpp
@@ -8,6 +8,9 @@
 IS::Procesor::Procesor()
 {
 	pravljenjeRegistara();
+	getInstructions();
+	ucitavanjePodataka();
+	statusMemorije();
 }
 
 IS::Procesor::~Procesor()
@@ -19,57 +22,75 @@ void IS::Procesor::getInstructions()
 	std::ifstream Fajl("a.txt");
 	for (std::string line; getline(Fajl, line);)
 		this->ListaInstrukcija.push_back(line);
+	static int d, b, t;
+	for (int i = 0; i < ListaInstrukcija.size(); i++)
+	{
+		if (!strcmp(ListaInstrukcija[i].c_str(), ".data")) 
+		{
+			d = i;
+			break;
+		}
+	}
+	for (int i = 0; i < ListaInstrukcija.size(); i++)
+	{
+		if (!strcmp(ListaInstrukcija[i].c_str(), ".bss"))
+		{
+			b = i;
+			break;
+		}
+	}
+	for (int i = 0; i < ListaInstrukcija.size(); i++)
+	{
+		if (!strcmp(ListaInstrukcija[i].c_str(), ".text")) 
+		{
+			t = i;
+			break;
+		}
+	}
+	try {
+		if (d < b && b < t)
+		{
+			for (int i = 0; d < b-1; d++, i++)
+				data.push_back(ListaInstrukcija[d + 1]);
+			for (int i = 0; b < t-1; b++, i++)
+				bss.push_back(ListaInstrukcija[b + 1]);
+			for (int i = 0; t < ListaInstrukcija.size()-1; t++, i++)
+				text.push_back(ListaInstrukcija[t + 1]);
+		}
+		else
+			throw;
+	}
+	catch(std::exception ex)
+	{
+		std::cout << "Greska u kodu!";
+	}
+
+}
+
+void IS::Procesor::ucitavanjePodataka()
+{
+	std::string line;
+	for (int i=0;i<data.size();i++)
+	{
+		line = data[i];
+		Instrukcija a(line);
+		long long temp = std::stoll(a.argument2, nullptr, 10);
+		this->memorija.insert({ a.argument1,temp });
+		a.~Instrukcija();
+	}
+	for (int i = 0; i < bss.size(); i++)
+	{
+		line = bss[i];
+		Instrukcija a(line);
+		this->memorija.insert({ a.argument1,0});
+		a.~Instrukcija();
+	}
 }
 
 void IS::Procesor::Izvrsavanje()
 {
-	getInstructions();
-	int t = this->provjeraSintakse();
-	if (t) 
-	{
-		for (std::string line = ListaInstrukcija[line_counter]; line_counter < ListaInstrukcija.size(); line_counter++)
-		{
-			if (!strcmp("BREAKPOINT", line.c_str())) {
-				char c;
-				std::cout << "Za next step unesi N, a za prelazak na sledeci BREAKPOINT unesi B." << std::endl;
-				std::cin >> c;
-				while (c == 'N')
-				{
-					line = ListaInstrukcija[++line_counter];
-					if (!strcmp(line.c_str(), "BREAKPOINT"))
-					{
-						line_counter--;
-						continue;
-					}
-					Instrukcija a(line);
-					IzvrsavanjeInstrukcije(a);
-					this->statusRegistara();
-					this->statusMemorije();
-					std::cout << "Za next step unesi N, a za prelazak na sledeci BREAKPOINT unesi B." << std::endl;
-					std::cin >> c;
-				}
-				if (c == 'B')
-				{
-					line_counter++;
-					while (strcmp(ListaInstrukcija[line_counter].c_str(), "BREAKPOINT") && line_counter + 1 < ListaInstrukcija.size())
-						line_counter++;
-					line_counter--;
-				}
 
-			}
-			else
-			{
-				Instrukcija a(line);
-				IzvrsavanjeInstrukcije(a);
-			}
-		}
-
-		std::cout << "Izvrsavanje zavrseno!" << std::endl;
-		statusMemorije();
-		statusRegistara();
-	}
-	else
-		std::cout << "Greska u kodu!" << std::endl;
+	
 }
 
 void IS::Procesor::IzvrsavanjeInstrukcije(Instrukcija a)
@@ -86,10 +107,6 @@ void IS::Procesor::IzvrsavanjeInstrukcije(Instrukcija a)
 					auto t1 = Registri.find(a.argument1);
 					auto t2 = Registri.find(a.argument2);
 					t1->second = t1->second + t2->second;
-				}
-				else if ()
-				{
-					
 				}
 				else
 				{
@@ -110,8 +127,8 @@ void IS::Procesor::IzvrsavanjeInstrukcije(Instrukcija a)
 
 void IS::Procesor::pravljenjeRegistara()
 {
-	this->Registri.insert({ "EAX", (long)3 });
-	this->Registri.insert({ "EBX", (long)5 });
+	this->Registri.insert({ "EAX", (long)0 });
+	this->Registri.insert({ "EBX", (long)0 });
 	this->Registri.insert({ "ECX", (long)0 });
 	this->Registri.insert({ "EDX", (long)0 });
 }
@@ -124,7 +141,7 @@ void IS::Procesor::statusRegistara()
 
 void IS::Procesor::statusMemorije()
 {
-	for (auto x : Memorija)
+	for (auto x : memorija)
 		std::cout << x.first << " " << x.second << std::endl;
 }
 
@@ -135,18 +152,7 @@ int IS::Procesor::provjeraSintakse()
 
 void IS::Procesor::pronadjiLabele()
 {
-	std::ifstream Fajl("a.txt");
-	for (std::string line; getline(Fajl, line);)
-		this->ListaInstrukcija.push_back(line);
-	for (int i=0; i< ListaInstrukcija.size();i++)
-	{
-		if (ListaInstrukcija[i].find(":") >= 0)
-		{
-			std::string temp = "";
-			temp = ListaInstrukcija[i].substr(0, ListaInstrukcija[i].size() - 1);
-			Labele.insert({ temp,i });
-		}
-	}
+	
 }
 
 void IS::Procesor::pokazivac(Instrukcija a)
@@ -162,9 +168,9 @@ void IS::Procesor::pokazivac(Instrukcija a)
 			std::string temp = std::to_string(t1->second);
 			a.argument1 = temp;
 		}
-		else if (Memorija.contains(a.argument1))
+		else if (memorija.contains(a.argument1))
 		{
-			auto t1 = Memorija.find(a.argument1);
+			auto t1 = memorija.find(a.argument1);
 			std::string temp = std::to_string(t1->second);
 			a.argument1 = temp;
 		}
@@ -180,9 +186,9 @@ void IS::Procesor::pokazivac(Instrukcija a)
 			std::string temp = std::to_string(t1->second);
 			a.argument2 = temp;
 		}
-		else if (Memorija.contains(a.argument2))
+		else if (memorija.contains(a.argument2))
 		{
-			auto t1 = Memorija.find(a.argument2);
+			auto t1 = memorija.find(a.argument2);
 			std::string temp = std::to_string(t1->second);
 			a.argument2 = temp;
 		}
